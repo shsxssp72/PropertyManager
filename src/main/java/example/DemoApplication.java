f70a283d345362870a894b13a0b21b5587ef5281
shsxssp72 <1260027595@qq.com>
2018-04-27 23:26:14 +0800

    Add Crypto Utilities.

--- src/main/java/example/DemoApplication.java
+++ src/main/java/example/DemoApplication.java
@@ -3,13 +3,23 @@ package example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.Property.Utility.CryptoUtil;
 
-@SpringBootApplication
-@MapperScan("example.CustomerMapper")
+import java.security.Key;
+
+//@SpringBootApplication
+//@MapperScan("example.CustomerMapper")
 public class DemoApplication
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication.run(DemoApplication.class, args);
+		CryptoUtil b=new CryptoUtil();
+		Key a=b.generateAESKey();
+		String hello="HelloWorld";
+		String c=b.AESEncrypt(hello,a);
+		System.out.println(c);
+		System.out.println(b.AESDecrypt(c,a));
+		System.out.println(b.Sha2Encoder(hello,b.getRandomString(4)));
 	}
 }
