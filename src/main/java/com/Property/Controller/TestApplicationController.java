e6bea97568e007e8288f2c2b370321997e41f107
shsxssp72 <1260027595@qq.com>
2018-05-03 22:13:48 +0800

    Added Swagger2

--- src/main/java/com/Property/Controller/TestApplicationController.java
+++ src/main/java/com/Property/Controller/TestApplicationController.java
@@ -3,19 +3,26 @@ package com.Property.Controller;
 import com.Property.Entity.Building;
 import com.Property.Entity.Ticket;
 import com.Property.Mapper.TicketMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
+@Api(value="/Test",tags="测试用页面")
 public class TestApplicationController
 {
 	@Autowired
 	private TicketMapper ticketMapper;
-	@RequestMapping("/Test")
+
+	@ApiOperation(value="测试",notes="仅作为测试用")
+	@RequestMapping(value="/Test",method=RequestMethod.GET)
 	public ModelAndView index()
 	{
 		List<Ticket> testLists=ticketMapper.getAll();
